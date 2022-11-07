385f8dff3539e4d26d6e2c29c2e4e8da28c04927
Utsav <utsavkumar5462@gmail.com>
2022-11-08 01:46:31 +0530

    print Hello World

---
+++ BasicsOfJava/src/getting_started_with_java/HelloWorld.java
@@ -0,0 +1,12 @@
+package getting_started_with_java;
+
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+    //	If you use println java will put a newline at the end.
+    //	And, if you use print java will not put a newline at the end.
+		System.out.println("Hello World");
+		System.out.print("Hello World");
+		System.out.println("Hello World");
+	}
+}
