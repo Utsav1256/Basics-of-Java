51dc4e8998a627010a5b5f4acb61ce99d3215827
Utsav <utsavkumar5462@gmail.com>
2022-11-08 02:09:29 +0530

    add two number

---
+++ BasicsOfJava/src/getting_started_with_java/Add2Num.java
@@ -0,0 +1,32 @@
+package getting_started_with_java;
+
+public class Add2Num {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 20;
+		
+		int sum = i + j;
+		System.out.println(sum);
+		
+		i = 17;
+		sum = i + j;
+		System.out.println(sum);
+		
+		short k = 123;
+		
+		long l = 12345;
+		
+		double d = 1.23;
+		
+		char c = 'a';
+		
+		boolean b = true;
+		boolean z = false;
+		
+//		float m;
+//		System.out.println(m);
+		
+	}
+
+}
