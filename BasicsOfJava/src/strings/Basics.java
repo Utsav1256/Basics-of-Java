6e4a5b39cd7f179cb81c2afdba574595734d53a2
Utsav <utsavkumar5462@gmail.com>
2023-01-03 19:12:32 +0530

    Create a String

---
+++ BasicsOfJava/src/strings/Basics.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		
+		// How we create a character
+		char c = 'c';
+		// How we create a string
+		String str = "abcdefg"; //this is stored in array as: |a|b|c|d|e|f|g|h|
+															// in character format
+		System.out.println(str);
+		
+	}
+
+}
