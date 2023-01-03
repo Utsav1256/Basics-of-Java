2e70732c0476e9deea5c4764abb5bf4ebf71a57e
Utsav <utsavkumar5462@gmail.com>
2023-01-03 16:28:29 +0530

    array for characters

--- BasicsOfJava/src/array/Basics.java
+++ BasicsOfJava/src/array/Basics.java
@@ -8,7 +8,16 @@ public class Basics {
 		int arr[] = new int[n];
 		arr[1] = 15;
 		System.out.println(arr[1]);
-		System.out.println(arr[0]);
+		System.out.println(arr[0]); // default value is 0 (for integers)
+		
+		char chArray[] = new char[n];
+		chArray[5] = 'a'; //"" -> dauble-quatation is used for string, not fore characters. 
+		                  //'' -> single-quatation is used for characters
+		System.out.println(chArray[5]);
+		System.out.println(chArray[6]); // default value is null (for characters)
+		chArray[6] = '!';
+		System.out.println(chArray[6]);
+		
 		
 	}
 
