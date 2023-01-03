7a415ad9a3270148de1650ba8b65bbe86a588430
Utsav <utsavkumar5462@gmail.com>
2023-01-03 16:16:59 +0530

    make a simple array

--- BasicsOfJava/src/array/Basics.java
+++ BasicsOfJava/src/array/Basics.java
@@ -4,11 +4,12 @@ public class Basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 10;
+		int n = 12;
 		int arr[] = new int[n];
 		arr[1] = 15;
 		System.out.println(arr[1]);
 		System.out.println(arr[0]);
+		
 	}
 
 }
