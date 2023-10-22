d577c823fa9c533115e2b428a00ed3c63ee814ea
Utsav <utsavkumar5462@gmail.com>
2023-10-22 20:28:35 +0530

    update

---
+++ src/Patterns/Pattern14.java
@@ -0,0 +1,38 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern14 {
+public static void main(String[] args) {
+		
+
+		/*
+	 	       1
+			  121
+			 12321
+			1234321
+		*/
+			
+	
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter number of rows for the square pattern : ");
+			int N = scan.nextInt();
+		
+			for(int i =1; i <= N; i++) {
+				int inc= 1;
+				int dec = i - 1;
+				for(int j = 1; j <= N-i; j++) {
+					System.out.print(" ");
+				}
+				for(int j = N - i + 1; j <= N; j++) {
+					System.out.print(inc);
+					inc++;
+				}
+				for(int j =  N + 1; j <= i + N - 1; j++) {
+					System.out.print(dec);
+					dec--;
+				} 
+				System.out.println();
+			}
+		} 
+}
