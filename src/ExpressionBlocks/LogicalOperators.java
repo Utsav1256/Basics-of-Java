1048713d293a0a8e323a6e7d6ebfc7a0f989c75e
Utsav1256 <utsavkumar5462@gmail.com>
2024-05-21 12:26:08 +0530

    LogicalOperators

---
+++ src/ExpressionBlocks/LogicalOperators.java
@@ -0,0 +1,27 @@
+package ExpressionBlocks;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+
+//		Logical Operators are used to check whether an expression is true / false.
+//		They are used in Descision making.
+		
+//		1. && (logical AND) expression1 && expression2
+//		true only if both exp1 and exp2 are true
+		System.out.println((5>3) && (6<7));
+		System.out.println((5<3) && (6<7));
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
