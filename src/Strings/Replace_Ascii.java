f72a50304cba995e5b5499d07ec92ebc1285cdd1
Utsav1256 <utsavkumar5462@gmail.com>
2024-07-08 14:11:07 +0530

    replce_ascii - StringBuilder

---
+++ src/Strings/Replace_Ascii.java
@@ -0,0 +1,44 @@
+package Strings;
+
+/*
+ - if the character at the even index, for current character you have to put the next character, in the same format (uppercase / lowercase).
+- if the character at the odd index, for current character you have to put the previous character, in the same format (uppercase / lowercase).
+
+For example:
+1.
+	"cdEfG" --> "dcFeH"
+2.
+	"dd" --> "ec"
+ */
+public class Replace_Ascii {
+	
+	public static String ascii_replacer(String str) {
+		StringBuilder sb = new StringBuilder(str);
+		
+		for(int i = 0; i < sb.length(); i++) {
+			char ch = sb.charAt(i);
+			if(i % 2 == 0) {
+				ch = (char)(ch+ 1);
+				sb.setCharAt(i, ch);
+			} else {
+				ch = (char)(ch - 1);
+				sb.setCharAt(i, ch);
+			}
+		}
+		str = sb.toString();
+		
+		return str;
+	}
+	public static void main(String[] args) {
+		String str = "cdEfG";
+		String str1 = "dd";
+		
+		String result = ascii_replacer(str);
+		String result1 = ascii_replacer(str1);
+		
+		System.out.println(str + " -> " + result);
+		System.out.println(str1 + " -> " + result1);
+
+	}
+
+}
