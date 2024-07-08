feafe880b54e92671b3d8e5e97d9c8c944ea8902
Utsav1256 <utsavkumar5462@gmail.com>
2024-07-08 14:38:22 +0530

    Struing intro

---
+++ src/Strings/Intro.java
@@ -0,0 +1,29 @@
+package Strings;
+
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String first = "Java";
+		String second = "Python";
+		String third = "JS";
+		// length(): Returns the length of the string.
+		System.out.println(first.length());
+		
+		// equals(): Compares the content of two strings.
+		String joinStr = first.concat(second);
+		System.out.println(joinStr);
+		
+		boolean res1 = first.equals(third);
+		System.out.println(res1);
+		
+		
+		
+		
+		
+
+	}
+
+}
