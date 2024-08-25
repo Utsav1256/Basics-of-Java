bb7415f3825ad2d2409cd5a3561acd748c869b2b
Utsav1256 <utsavkumar5462@gmail.com>
2024-08-26 02:33:09 +0530

    Implemented Inorder Traversal

--- src/Tree/BinaryTreeClient.java
+++ src/Tree/BinaryTreeClient.java
@@ -12,36 +12,41 @@ public class BinaryTreeClient {
 		preorderTraversal(root.right);
 	}
 	
+	static void inorderTrapersal(Node root) {
+		if(root == null) {
+			return;
+		}
+		
+		inorderTrapersal(root.left);
+		System.out.print(root.data + " ");
+		inorderTrapersal(root.right);
+	}
+	
 	public static void main(String[] args) {
 		BinaryTree b = new BinaryTree();
 		
 		b.root = new Node(10);
-		preorderTraversal(b.root);
-		System.out.println();
 		b.root.left = new Node(20);
-		preorderTraversal(b.root);
-		System.out.println();
 		b.root.right = new Node(30);
-		preorderTraversal(b.root);
-		System.out.println();
 		b.root.left.left = new Node(40);
-		preorderTraversal(b.root);
-		System.out.println();
 		b.root.left.right = new Node(50);
+		b.root.right.right = new Node(60);
+		
+		System.out.println("Preorder Traversal :)");
 		preorderTraversal(b.root);
 		System.out.println();
-		b.root.right.right = new Node(60);
-		preorderTraversal(b.root);
+		System.out.println("Inorder Traversal ;)");
+		inorderTrapersal(b.root);
 		System.out.println();
 		
 /* 
 output:
-10 
-10 20 
-10 20 30 
-10 20 40 30 
-10 20 40 50 30 
+
+Preorder Traversal :)
 10 20 40 50 30 60 
+Inorder Traversal ;)
+40 20 50 10 30 60 
+
 */
 
 	}
