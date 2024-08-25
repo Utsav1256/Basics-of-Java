6b10d149806f88f6e0b3c5b87aee402c6409ba6c
Utsav1256 <utsavkumar5462@gmail.com>
2024-08-26 02:49:57 +0530

    Minor update

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -2,4 +2,34 @@ package Tree;
 
 public class BinaryTree {
 	public Node root;
+	
+	public void preorderTraversal(Node root) {
+		if(root == null) {
+			return;
+		}
+		
+		System.out.print(root.data + " ");
+		preorderTraversal(root.left);
+		preorderTraversal(root.right);
+	}
+	
+	public void inorderTrapersal(Node root) {
+		if(root == null) {
+			return;
+		}
+		
+		inorderTrapersal(root.left);
+		System.out.print(root.data + " ");
+		inorderTrapersal(root.right);
+	}
+	
+	public void postorderTraversal(Node root) {
+		if(root == null) {
+			return;
+		}
+		
+		postorderTraversal(root.left);
+		postorderTraversal(root.right);
+		System.out.print(root.data + " ");
+	}
 }
--- src/Tree/BinaryTreeClient.java
+++ src/Tree/BinaryTreeClient.java
@@ -2,35 +2,6 @@ package Tree;
 
 public class BinaryTreeClient {
  
-	static void preorderTraversal(Node root) {
-		if(root == null) {
-			return;
-		}
-		
-		System.out.print(root.data + " ");
-		preorderTraversal(root.left);
-		preorderTraversal(root.right);
-	}
-	
-	static void inorderTrapersal(Node root) {
-		if(root == null) {
-			return;
-		}
-		
-		inorderTrapersal(root.left);
-		System.out.print(root.data + " ");
-		inorderTrapersal(root.right);
-	}
-	
-	static void postorderTraversal(Node root) {
-		if(root == null) {
-			return;
-		}
-		
-		postorderTraversal(root.left);
-		postorderTraversal(root.right);
-		System.out.print(root.data + " ");
-	}
 	
 	public static void main(String[] args) {
 		BinaryTree b = new BinaryTree();
@@ -43,13 +14,13 @@ public class BinaryTreeClient {
 		b.root.right.right = new Node(60);
 		
 		System.out.println("Preorder Traversal :)");
-		preorderTraversal(b.root);
+		b.preorderTraversal(b.root);
 		System.out.println();
 		System.out.println("Inorder Traversal ;)");
-		inorderTrapersal(b.root);
+		b.inorderTrapersal(b.root);
 		System.out.println();
 		System.out.println("Postorder Traversal :]");
-		postorderTraversal(b.root);
+		b.postorderTraversal(b.root);
 		System.out.println();
 		
 /* 
@@ -59,6 +30,8 @@ Preorder Traversal :)
 10 20 40 50 30 60 
 Inorder Traversal ;)
 40 20 50 10 30 60 
+Postorder Traversal :]
+40 50 20 60 30 10 
 
 */
 
