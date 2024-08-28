d711581773aaa20d4dc093863e87db124cbf6a9f
Utsav1256 <utsavkumar5462@gmail.com>
2024-08-28 14:58:21 +0530

    created simple Linked list

---
+++ src/LinkedList_Camp/LinkedListUse.java
@@ -0,0 +1,14 @@
+package LinkedList_Camp;
+
+public class LinkedListUse {
+
+	public static void main(String[] args) {
+		Node n1 = new Node();
+		System.out.println(n1); // -> LinkedList_Camp.Node@b1bc7ed
+		
+		System.out.println(n1.data); // -> 0
+		System.out.println(n1.next); // -> Null
+
+	}
+
+}
---
+++ src/LinkedList_Camp/Node.java
@@ -0,0 +1,7 @@
+package LinkedList_Camp;
+
+public class Node {
+	int data;
+	Node next;
+
+}
