4fad9d1e0f6506b63fd870c2daa8dfc2de2048c2
Utsav1256 <utsavkumar5462@gmail.com>
2024-08-28 15:17:52 +0530

    Converting the Node class to be Generic

--- src/LinkedList_Camp/LinkedListUse.java
+++ src/LinkedList_Camp/LinkedListUse.java
@@ -3,7 +3,7 @@ package LinkedList_Camp;
 public class LinkedListUse {
 
 	public static void main(String[] args) {
-		Node n1 = new Node(10);
+		Node<Integer> n1 = new Node<>(10);
 		System.out.println(n1); // -> LinkedList_Camp.Node@b1bc7ed
 		
 		System.out.println(n1.data); // -> 10
--- src/LinkedList_Camp/Node.java
+++ src/LinkedList_Camp/Node.java
@@ -1,10 +1,10 @@
 package LinkedList_Camp;
 
-public class Node {
-	int data;
-	Node next;
+public class Node<T> {
+	T data;
+	Node<T> next;
 	
-	Node(int data) {
+	Node(T data) {
 		this.data =  data;
 //		this.next = null; -> no need to write this bcz default value of the Node type will be `null`
 	}
