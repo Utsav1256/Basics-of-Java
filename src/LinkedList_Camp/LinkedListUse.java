1d3d69b70cf51bfa89f6b1c739ba96a2fb647f0b
Utsav1256 <utsavkumar5462@gmail.com>
2024-08-28 15:03:33 +0530

    Linked list created using Parametrized constructor

--- src/LinkedList_Camp/LinkedListUse.java
+++ src/LinkedList_Camp/LinkedListUse.java
@@ -3,10 +3,10 @@ package LinkedList_Camp;
 public class LinkedListUse {
 
 	public static void main(String[] args) {
-		Node n1 = new Node();
+		Node n1 = new Node(10);
 		System.out.println(n1); // -> LinkedList_Camp.Node@b1bc7ed
 		
-		System.out.println(n1.data); // -> 0
+		System.out.println(n1.data); // -> 10
 		System.out.println(n1.next); // -> Null
 
 	}
--- src/LinkedList_Camp/Node.java
+++ src/LinkedList_Camp/Node.java
@@ -3,5 +3,9 @@ package LinkedList_Camp;
 public class Node {
 	int data;
 	Node next;
-
+	
+	Node(int data) {
+		this.data =  data;
+//		this.next = null; -> no need to write this bcz default value of the Node type will be `null`
+	}
 }
