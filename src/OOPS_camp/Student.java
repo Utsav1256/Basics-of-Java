c80253628521c40077603eba786997999a53597f
Utsav1256 <utsavkumar5462@gmail.com>
2024-08-29 11:19:12 +0530

    Student object creation

---
+++ src/OOPS_camp/Student.java
@@ -0,0 +1,6 @@
+package OOPS_camp;
+
+public class Student {
+	String name;
+	int rollNumber;
+}
---
+++ src/OOPS_camp/StudentUse.java
@@ -0,0 +1,10 @@
+package OOPS_camp;
+
+public class StudentUse {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println(s1); //-> OOPS_camp.Student@b1bc7ed
+	}
+
+}
